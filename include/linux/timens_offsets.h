a3e01e08aeca86ee84c12466d0a9f43364902e16
Dmitry Safonov <dima@arista.com>
2018-08-16 15:51:39 +0100

    (Drop?) timens: Optimize zero-offsets
    Fallthrough on host or in ns without time set.
    
    Signed-off-by: Dmitry Safonov <dima@arista.com>

--- include/linux/timens_offsets.h
+++ include/linux/timens_offsets.h
@@ -1,6 +1,13 @@
 #ifndef _LINUX_TIME_OFFSETS_H
 #define _LINUX_TIME_OFFSETS_H
 
+enum {
+	/* We're in namespace - add offsets from vvar */
+	TIMENS_USE_OFFSETS	= 1,
+	/* Don't expose host's offsets, fall back to syscall - slow */
+	TIMENS_FALLBACK_SYSCALL	= 2, /* TODO if anyone actually interested */
+};
+
 /*
  * Time offsets need align as they're placed on vvar page,
  * which should have tail paddings on compatible vdso.
@@ -8,6 +15,7 @@
  * to timespec because of a padding occuring between the fields.
  */
 struct timens_offsets {
+	unsigned long flags;
 	struct timespec64  monotonic_time_offset __aligned(8);
 	struct timespec64  monotonic_boottime_offset __aligned(8);
 };
