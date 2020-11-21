4720853f6d5583966b8ee08cc49ae801325f26b8
Andrei Vagin <avagin@gmail.com>
2020-11-21 01:10:58 -0800

    [RFC] mm: allow temporary switching mm and run in its context
    This change introduces a new system call:
    
    long process_vm_exec(pid_t, pid, struct sigcontext uctx, unsigned long flags)
    
    This syscall call changes the current address space on an address space
    of a specified task and run the current process with registers provided
    in uctx. It returns back on any system call or signal. uctx will
    contains registers that the task had right before it was stopped.
    
    process_vm_exec returns:
    * 0 if a system call has been called.
    * a signal number if it has been interrupted by signal.
    * a negative value in case of any error.
    
    Signed-off-by: Andrei Vagin <avagin@gmail.com>

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -1347,4 +1347,6 @@ int __sys_getsockopt(int fd, int level, int optname, char __user *optval,
 		int __user *optlen);
 int __sys_setsockopt(int fd, int level, int optname, char __user *optval,
 		int optlen);
+
+void restore_vm_exec_context(struct pt_regs *regs);
 #endif
